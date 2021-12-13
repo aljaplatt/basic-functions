9e415bc9a826adfd83e895ae1a419b0a57f7c13a
Alex Platt <81518354+aljaplatt@users.noreply.github.com>
2021-12-13 17:14:28 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+/** 
+ * before starting: remember to copy the path for index.html into the browser, which means you can try 
+ * out your functions in the console.  
+ * you do not need to make any changes in index.html this is just to allow you to see your output 
+ * in the console
+ */
+
+/**
+ * Task 1: create a function that will take in a name and say hello to that person in the console
+ */
+
+/**
+ * Task 2: create a function that will take in a number, and say I can count to and the number in the console
+ */
+
+/**
+ * Task 3: create a function that takes in a word, and returns that word with " is a word" added to it 
+ */
+
+/**
+ * Task 4: create a function that will take in a number, and if it is higher than 5, say "high number" in the console
+ */
+
+/**
+ * Task 5: create a function that will take in a number, and if it is lower than 5, say "low number" in the console
+ */
+
+/**
+ * Task 6: create a function that will take in a number, and if it is even, log even in the console, or log odd if not
+ */
+
+/**
+ * Task 7: create a function that will loop from 0 to 10 and show each number in the console 
+ */
+
+/**
+ * Task 8: create a function that takes in a word, and will loop through each letter, logging it in the console
+ */
+
+/**
+ * Task 9: create a function that will loop from 0 to 100, and log every even number
+ */
+
+/*
+ * Task 10: complete the below function for this problem:
+ * Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts
+ * the number of sheep present in the array (true means present).
+ *
+ * For example,
+ * 
+ * [true,  true,  true,  false]
+ * The correct answer would be 3.
+ */
+
+function countSheep(sheepArray) {
+
+}
